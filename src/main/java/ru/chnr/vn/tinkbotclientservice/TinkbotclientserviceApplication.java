134457cfd69c97cd21fb7fca59e069ee2e8f7b98
Ivan <rockachun19@gmail.com>
2022-08-06 15:50:26 +0300

    Initial commit

---
+++ src/main/java/ru/chnr/vn/tinkbotclientservice/TinkbotclientserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.chnr.vn.tinkbotclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkbotclientserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkbotclientserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/chnr/vn/tinkbotclientservice/TinkbotclientserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chnr.vn.tinkbotclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkbotclientserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
