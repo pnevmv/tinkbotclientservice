e5831dfd69f595d2fb54701d6c934916c81bdeb0
Ivan <rockachun19@gmail.com>
2022-08-16 14:33:55 +0300

    0.0.2 grpc connect

---
+++ src/main/java/ru/chnr/vn/tinkbotclientservice/configure/CheckGrpc.java
@@ -0,0 +1,20 @@
+package ru.chnr.vn.tinkbotclientservice.configure;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.chnr.vn.tinkbotclientservice.controller.BotService;
+
+@Configuration
+public class CheckGrpc {
+    private static final Logger log = LoggerFactory.getLogger(CheckGrpc.class);
+
+    @Bean
+    CommandLineRunner initDatabase(BotService botService) {
+        return args -> {
+            log.info("Bot id response: " + botService.createBot(228));
+        };
+    }
+}
---
+++ src/main/java/ru/chnr/vn/tinkbotclientservice/controller/BotService.java
@@ -0,0 +1,29 @@
+package ru.chnr.vn.tinkbotclientservice.controller;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+
+import org.springframework.stereotype.Service;
+import ru.chnr.vn.common.generated.BotClientGrpc;
+import ru.chnr.vn.common.generated.Token;
+
+@Service
+public class BotService {
+
+    @GrpcClient("botService")
+    private BotClientGrpc.BotClientBlockingStub botClientBlockingStub;
+
+    public long createBot(long t){
+        Token token = Token
+                .newBuilder()
+                .setToken(t)
+                .build();
+
+        long response = botClientBlockingStub
+                .createBot(token)
+                .getBotID();
+
+        System.out.println("new bot id " + response);
+
+        return response;
+    }
+}
